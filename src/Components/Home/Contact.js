668b0e40b1173190b86fb8126190a0a006caf9d4
aimunhidden485 <aimunnaharr@gmail.com>
2022-06-09 20:41:32 -0700

    contact me page implemented

--- src/Components/Home/Contact.js
+++ src/Components/Home/Contact.js
@@ -5,18 +5,27 @@ const Contact = () => {
         e.preventDefault()
     }
     return (
-        <div className='mb-5'>
-            <form onSubmit={handleSubmit} action="mailto:aimunnaharr@gmail.com" method='POST' enctype="multipart/form-data">
+        <div className='mb-5  w-[300px] mx-auto '>
+            <h1 className='text-center font-bold'>Contact Me</h1>
+            <form className='w-full mx-auto' onSubmit={handleSubmit} action="https://formsubmit.co/bca297d6b8769d7e0fe891d12114cec5" method="POST">
             <div class="form-control w-full max-w-xs">
   <label class="label">
-    <span class="label-text">What is your name?</span>
-    
+    <span class="label-text">Email</span>
+   
   </label>
-  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
- 
+  <input type="email" placeholder="Email" class="input input-bordered w-full max-w-xs" required/>
+  
 </div>
-<input className='btn btn-primary' type='submit'/>
-            </form>
+            <div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">Message</span>
+   
+  </label>
+  <input type="text" placeholder="Message" class="input input-bordered w-full max-w-xs" required />
+  
+</div><br/>
+     <button className='btn btn-primary' type="submit">Send</button>
+</form>
         </div>
     );
 };
