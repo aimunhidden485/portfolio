9291853960cb36cdc7aa60a97bf42ab5ed33d4b0
aimunhidden485 <aimunnaharr@gmail.com>
2022-06-10 20:50:46 -0700

    blog resume and details routed creating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import Blogs from './Components/Blogs';
+import Resume from './Components/Resume';
 
 function App() {
   return (
     <div>
       
       <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs/>}></Route>
+        <Route path='/resume' element={<Resume/>}></Route>
+        <Route path='/brush' element={<Home></Home>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
     </div>
---
+++ src/Components/BrushDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BrushDetails = () => {
+    return (
+        <div>
+            <h1>This is brush website details</h1>
+        </div>
+    );
+};
+
+export default BrushDetails;
