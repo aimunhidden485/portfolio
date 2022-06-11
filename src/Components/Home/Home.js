bdca1e7ec0ed593575bc31d8751eda0432cd7205
aimunhidden485 <aimunnaharr@gmail.com>
2022-06-10 19:41:50 -0700

    navbar added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from '../../Shared/Navbar';
 import Banner from './Banner';
 import Contact from './Contact';
 import Projects from './Projects';
@@ -6,7 +7,7 @@ import Projects from './Projects';
 const Home = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
             <Banner></Banner>
             <Projects></Projects>
             <Contact></Contact>
